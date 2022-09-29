1fb10a39bf43309bffb71dc29f51d539e964367d
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-09-29 15:12:35 +0000

    [Sync]: Runtime - 167 ms (5.29%), Memory - 36.4 MB (60.36%)

--- 0658-find-k-closest-elements/solution.cpp
+++ 0658-find-k-closest-elements/solution.cpp
@@ -5,7 +5,21 @@ public:
         int n=arr.size();
         vector<pair<int,int>> v;
         vector<int> ans;
-        for(int i=0; i<n; i++){
+        int m=0;
+        int y=n;
+        auto it =lower_bound(arr.begin(),arr.end(),x);
+        if((it-arr.begin())+k>=n){
+            y=n;
+        } else {
+            y=(it-arr.begin())+k+1;
+        }
+         if((it-arr.begin())-k<=n){
+            m=0;
+        } else{
+             x=abs((it-arr.begin())-k);
+         }
+        
+        for(int i=m; i<y; i++){
             pair<int,int> p;
             p.second = arr[i];
             p.first =abs(arr[i]-x);
