00a68c24d23a28a7eb0ac183366b5fa79a122716
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-11-01 07:57:24 +0000

    [Sync]: Runtime - 72 ms (2.68%), Memory - 25 MB (100.00%)

--- 0189-rotate-array/solution.cpp
+++ 0189-rotate-array/solution.cpp
@@ -3,11 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int size = nums.size();
         k = (k)%size;
-        vector<int> temp;
-        for(int i=0; i<size; i++){
-            temp.push_back(nums[(i+size-k)%size]);
-            // cout<<temp[i]<<" ";
-        }
-        nums=temp;
+        reverse(nums.begin()+size-k,nums.end());      
+        reverse(nums.begin(),nums.end()-k); 
+        reverse(nums.begin(),nums.end());
     }
 };
