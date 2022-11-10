d5466f7c4dd1d029d91b177fcc321bd49e408edb
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-11-10 16:56:00 +0000

    [Sync]: Runtime - 14 ms (5.29%), Memory - 10.3 MB (100.00%)

--- 0350-intersection-of-two-arrays-ii/solution.cpp
+++ 0350-intersection-of-two-arrays-ii/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int> m;
+        unordered_map<int,int> m;
         vector<int> ans;
         for(int i=0; i<nums1.size(); i++){
             m[nums1[i]]++;
