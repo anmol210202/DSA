fa55f050108a55f2b8e5dcacb15ff190f59f9b8a
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-11-11 16:36:55 +0000

    [Sync]: Runtime - 343 ms (6.83%), Memory - 93.4 MB (99.98%)

--- 0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ 0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -5,7 +5,7 @@ public:
         int ans = 0;
         for(int i=0; i<prices.size(); i++){
             mi = min(mi,prices[i]);
-            if(prices[i]-mi>ans) ans = prices[i]-mi;
+            ans = max(ans,prices[i]-mi);
         }
         return ans;
     }
