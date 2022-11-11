e4153b00b09cbe73f7f0a339663e06be09ee1432
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-11-11 17:06:37 +0000

    [Sync]: Runtime - 28 ms (4.02%), Memory - 10.6 MB (100.00%)

--- 0566-reshape-the-matrix/solution.cpp
+++ 0566-reshape-the-matrix/solution.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        int x = mat.size();
-        int y = mat[0].size();
-        if(r*c!=x*y) return mat;
-        vector<vector<int>> ans;
-        vector<int> temp;
-        vector<int> odm;
-        for(int i =0; i<x; i++){
-            for(int j=0; j<y; j++){
-                odm.push_back(mat[i][j]);
-            }
-        }
-        for(int i=0; i<odm.size(); i++){
-            if((i+1)%c==0){
-                temp.push_back(odm[i]);
-                ans.push_back(temp);
-                temp.clear();
-            } else{
-                temp.push_back(odm[i]);
-            }
-        }
-        return ans;
+       int r0 = mat.size(), c0 = mat[0].size();
+        if (r0*c0 != r*c) return mat;
+        vector<int> v0(c);
+        vector<vector<int>> v(r, v0);
+        for (int i=0; i<r0*c0; ++i) v[i/c][i%c] = mat[i/c0][i%c0];
+        return v;
     }
 };
