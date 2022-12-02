7763a79085694f1b94a7ba08f5ec80bd518411a3
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-12-02 17:07:15 +0000

    [Sync]: Runtime - 373 ms (5.08%), Memory - 20.8 MB (99.49%)

--- 1777-determine-if-two-strings-are-close/solution.cpp
+++ 1777-determine-if-two-strings-are-close/solution.cpp
@@ -2,29 +2,18 @@ class Solution {
 public:
     bool closeStrings(string word1, string word2) {
         int l1 = word1.size() , l2 = word2.size();
-        if(l1!=l2) return false ;
-        unordered_map<char,int> m1,m2;
-        for(auto &e : word1){
-            m1[e]++;
+        if(l1!=l2) return false;
+        set<char> s1,s2;
+        vector<int> v1(26,0) , v2(26,0);
+        for(int i=0; i<l1; i++){
+            s1.insert(word1[i]);
+            s2.insert(word2[i]);
+            v1[word1[i]-'a']++;
+            v2[word2[i]-'a']++;
         }
-        for(auto &e : word2){
-            m2[e]++;
-        }
-        vector<char> c,C;
-        vector<int> i,I;
-        for(auto &e:m1){
-            c.push_back(e.first);
-            i.push_back(e.second);
-        }
-        sort(c.begin(),c.end());
-        sort(i.begin(),i.end());
-        for(auto &e:m2){
-            C.push_back(e.first);
-            I.push_back(e.second);
-        }
-        sort(C.begin(),C.end());
-        sort(I.begin(),I.end());
-        if(c == C and i == I) return true;
+        sort(v1.begin(),v1.end());
+        sort(v2.begin(),v2.end());
+        if(s1==s2 and v1==v2) return true;
         return false;
     }
 };
