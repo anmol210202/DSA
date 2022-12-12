c35cb1c9c5f70c218ac584dc7d701fe66ed91822
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-12-12 15:34:11 +0000

    [Sync]: Runtime - 3 ms (3.75%), Memory - 5.9 MB (100.00%)

--- 0231-power-of-two/solution.cpp
+++ 0231-power-of-two/solution.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<=0) return false;
+        return (n&(n-1))==0;
         int c=0;
         for(int i=0; i<sizeof(int)*8; i++){
             if(n&(1<<i)) c++;
