5ec1d085c72ca16979f6c03708fd8ad979f1402e
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-12-12 15:50:02 +0000

    [Sync]: Runtime - 14 ms (6.12%), Memory - 10.8 MB (100.00%)

--- 0414-third-maximum-number/solution.cpp
+++ 0414-third-maximum-number/solution.cpp
@@ -4,6 +4,12 @@ public:
         set<int> s;
         for(auto &e:nums) s.insert(e);
         if(s.size()<3) return *--s.end();
-        return *next(s.rbegin(),2);
+        auto it = s.rbegin();
+        advance(it,2);
+        return *it;
+        // return *next(s.rbegin(),2);
+        // *--s.end() or *s.rbegin()      -------- s.end() return close to last itr
+        // it = s.begin();
+        //advance(it,2);
     }
 };
