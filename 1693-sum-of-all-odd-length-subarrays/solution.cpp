e2fd0467c542668e7d1e7a767cb1c602578ec43c
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-01-06 17:31:29 +0000

    [Sync]: Runtime - 5 ms (6.65%), Memory - 8.3 MB (100.00%)

--- 1693-sum-of-all-odd-length-subarrays/solution.cpp
+++ 1693-sum-of-all-odd-length-subarrays/solution.cpp
@@ -3,11 +3,7 @@ public:
     int sumOddLengthSubarrays(vector<int>& arr) {
         int s=arr.size() , a=0,temp=0;
         for(int i=0; i<s; i++){
-            temp = 0;
-            for(int j=i;j<s; j++){
-                temp+=arr[j];
-                if((i+j)%2==0) a+=temp;
-            }
+            a+=(arr[i]*ceil((i+1)*(s-i)/2.0));
         }
         return a;
     }
