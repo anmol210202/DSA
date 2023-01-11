c18570bac62d008cf9cd9cef1d3adf2e8c2ecb2b
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-01-11 18:11:01 +0000

    [Sync]: Runtime - 176 ms (5.21%), Memory - 60.4 MB (100.00%)

--- 1554-minimum-time-to-collect-all-apples-in-a-tree/solution.cpp
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/solution.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> g;
+    vector<vector<int>> v;
     int dfs(int u,int p,vector<bool>& hasApple){
-     int res = 0, t = 0;
-            for (auto v : g[u]) {
-                if (p ^ v) {
-                    t = dfs(v, u,hasApple);
-                    if (t > 0 || hasApple[v]) {
-                        res += t + 2;
-                    }
+     int ans = 0, temp = 0;
+            for (auto e : v[u]) {
+                if (p!=e) {
+                    temp = dfs(e, u,hasApple);
+                    if (temp > 0 || hasApple[e]) ans += temp + 2;
                 }
             }
-            return res;
+            return ans;
     }
 
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
-        g.resize(n);
+        v.resize(n);
         for(auto &e:edges){
-            g[e[0]].push_back(e[1]);
-            g[e[1]].push_back(e[0]);            
+            v[e[0]].push_back(e[1]);
+            v[e[1]].push_back(e[0]);            
         }
         return dfs(0,-1,hasApple);
     }
