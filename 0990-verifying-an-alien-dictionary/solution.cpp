1fa5f8e474a3bdbd40fbbf2e45faf9ac58988c45
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-02-02 15:47:33 +0000

    [Sync]: Runtime - 6 ms (3.86%), Memory - 9.5 MB (100.00%)

--- 0990-verifying-an-alien-dictionary/solution.cpp
+++ 0990-verifying-an-alien-dictionary/solution.cpp
@@ -4,8 +4,6 @@ public:
         vector<int> v(26,0);
         for(int i=0; i<order.size(); i++) v[order[i]-'a']=i;
         for(auto &e:words) for(auto &x:e) x=v[x-'a']+'a';
-        vector<string> copy(words);
-        sort(copy.begin(),copy.end());
-        return copy==words ? true:false;
+        return is_sorted(words.begin(),words.end());
     }
 };
