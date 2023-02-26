4e2fe4644f62096aac73c019ed465992fb72bdfd
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-02-26 09:12:36 +0000

    [Sync]: Runtime - 23 ms (9.64%), Memory - 19.3 MB (100.00%)

--- 0283-move-zeroes/solution.cpp
+++ 0283-move-zeroes/solution.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=0,j=1,s=nums.size();
-        while(i<s and j<s){
-            if(nums[i]==0 and nums[j]!=0){
-                swap(nums[i],nums[j]);
-                i++;
-                j++;
-            }
-            else if(nums[i]==0 and nums[j]==0) j++;
-            else if(nums[i]==0 and nums[j]!=0){
-                i++; j++;
-            }
-            else {
-                i++;
-                j++;
-            }
-        }
+        int i=0;
+        for(auto &e:nums) if(e!=0) swap(e,nums[i]),i++;
     }
 };
