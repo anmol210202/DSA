1237459a52ccf737a291a1a8f1ea1cf5bc2869c0
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-03-12 17:50:48 +0000

    [Sync]: Runtime - 2 ms (16.01%), Memory - 6.7 MB (100.00%)

--- 0118-pascals-triangle/solution.cpp
+++ 0118-pascals-triangle/solution.cpp
@@ -1,37 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<int> temp;
-        vector<vector<int>> ans;
-         if(numRows == 1){
-             temp.push_back(1);
-            ans.push_back(temp);
-            return ans;
-        }
-        if(numRows == 2){
-             temp.push_back(1);
-            ans.push_back(temp);
-            temp.clear();
-            temp.push_back(1);
-            temp.push_back(1);
-            ans.push_back(temp);
-            return ans;
-        }
-         temp.push_back(1);
-            ans.push_back(temp);
-            temp.clear();
-            temp.push_back(1);
-            temp.push_back(1);
-            ans.push_back(temp);
-        temp.clear();
-        for(int i=2; i<numRows; i++){
-            temp.push_back(1);
-            for(int j=1; j<i; j++){
-                temp.push_back(ans[i-1][j-1]+ans[i-1][j]);
-            }
-            temp.push_back(1);
-            ans.push_back(temp);
-            temp.clear();
+        vector<vector<int>> ans(numRows);
+        for(int i=0; i<numRows; i++){
+            ans[i].resize(i+1,1);
+            for(int j=1; j<i; j++) ans[i][j]=ans[i-1][j] + ans[i-1][j-1]; 
         }
         return ans;
     }
