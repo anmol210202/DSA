5457f3852847d68eba7d214bc0408fddaa219300
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-03-12 19:10:29 +0000

    [Sync]: Runtime - 3 ms (3.10%), Memory - 12.1 MB (100.00%)

--- 0031-next-permutation/solution.cpp
+++ 0031-next-permutation/solution.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int i = nums.size() - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
+        auto pivot = std::is_sorted_until(nums.rbegin(), nums.rend());
+        if (pivot != nums.rend()) {
+            auto change = std::upper_bound(nums.rbegin(), pivot, *pivot);
+            std::swap(*pivot, *change);
         }
-        if (i >= 0) {
-            int j = nums.size() - 1;
-            while (j >= 0 && nums[j] <= nums[i]) {
-                j--;
-            }
-            swap(nums[i], nums[j]);
-        }
-        reverse(nums.begin() + i + 1, nums.end());
+        std::reverse(nums.rbegin(), pivot);
     }
 };
 
