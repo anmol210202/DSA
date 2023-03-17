e1364aaf802a2be9bb618086d06fc2bca43409ce
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-03-17 17:47:38 +0000

    [Sync]: Runtime - 112 ms (9.79%), Memory - 61.3 MB (100.00%)

--- 0287-find-the-duplicate-number/solution.cpp
+++ 0287-find-the-duplicate-number/solution.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int,int> m;
-        for(auto &e:nums) m[e]++;
-        for(auto &e:m) if(e.second!=1) return e.first;
-        return -1;
+        int slow=nums[0],fast=nums[0];
+        while(true){
+            slow=nums[slow],fast=nums[nums[fast]];
+            if(slow==fast) break;
+        }
+        slow = nums[0];
+        while(slow!=fast)
+            slow=nums[slow],fast=nums[fast];
+        return slow;
     }
 };
