a4c1c5bb8e90e629e779a8e0eb441af7f698d602
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-03-21 11:30:09 +0000

    [Sync]: Runtime - 531 ms (5.01%), Memory - 45.7 MB (99.64%)

--- 0128-longest-consecutive-sequence/solution.cpp
+++ 0128-longest-consecutive-sequence/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> s;
-        for(auto &e:nums) s.insert(e);
+        unordered_set<int> s(nums.begin(),nums.end());
+        // for(auto &e:nums) s.insert(e);
         int maxl=0;
         for(auto &e:nums)
             if(s.find(e-1)==s.end()){
