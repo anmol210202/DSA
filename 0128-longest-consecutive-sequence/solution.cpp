2518dc97a4609414a5b5dfb4476084d4220f2e29
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-03-21 11:26:30 +0000

    [Sync]: Runtime - 561 ms (5.01%), Memory - 49.7 MB (99.64%)

--- 0128-longest-consecutive-sequence/solution.cpp
+++ 0128-longest-consecutive-sequence/solution.cpp
@@ -4,18 +4,12 @@ public:
         unordered_set<int> s;
         for(auto &e:nums) s.insert(e);
         int maxl=0;
-        for(auto &e:nums){
+        for(auto &e:nums)
             if(s.find(e-1)==s.end()){
                 int tmpl=1;
-                while(s.find(e+tmpl-1)!=s.end()){
-                    tmpl++;
-                    // cout<<e<<' ';
-                }
-                // cout<<endl;
-                // cout<<tmpl<<' '<<endl;;
-                maxl=max(maxl,tmpl-1);
+                while(s.find(e+tmpl)!=s.end()) tmpl++;
+                maxl=max(maxl,tmpl);
             }
-        }
         return maxl;
     }
 };
