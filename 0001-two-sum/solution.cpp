8943644de45a171b442af2290605d8a554f16e39
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-03-22 11:18:20 +0000

    [Sync]: Runtime - 10 ms (46.07%), Memory - 11 MB (99.99%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -1,57 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> v(n);
-        for(int i = 0; i < n; i++) {
-            v[i] = nums[i];
-        }
-        sort(nums.begin(), nums.end());
-        int first_posi, second_posi;
-        pair<int, bool> p;
-        for(int i = 0; i < n; i++) {
-            int find_element = target  - nums[i];
-            p = check(n, nums, find_element);
-            if(p.second) {
-                for(int j = 0; j < n; j++) {
-                    if(nums[i] == v[j]) {
-                        first_posi = j;
-                        break;
-                    }
-                }
-                for(int j = n - 1; j >= 0; j--) {
-                    if(p.first == v[j] && j != first_posi) {
-                        second_posi = j;
-                        break;
-                    }
-                }
-                v.clear();
-                v.insert(v.end(),{first_posi,second_posi});
-                sort(v.begin(), v.end());
-                return v;
+        int i=0,j=nums.size()-1;
+        vector<pair<int,int>> vv;
+        for(auto i=0; i<nums.size();i++) vv.push_back({nums[i],i});
+        sort(vv.begin(),vv.end());
+        vector<int> v;
+        while(i<j){
+            if(vv[i].first+vv[j].first==target){
+                v.push_back(vv[i].second),v.push_back(vv[j].second);
+                break;
             }
+            else if(vv[i].first+vv[j].first>target) j--;
+            else i++;
         }
-        return  v;
-    }
-    
-    pair<int, bool> check(int n, vector<int> &nums, int find_element) {
-        pair<int, int> p;
-        int first = 0, last = n - 1;
-        while(first <= last) {
-            int mid = first + ((last - first) / 2);
-            
-            if(nums[mid] == find_element)  {
-                p.first = nums[mid];
-                p.second = true;
-                return p;
-            }
-            else if(nums[mid] > find_element) 
-                    last = mid - 1;
-            else 
-                    first = mid + 1;
-        }
-        p.first = -1;
-        p.second = false;
-        return p;
+        return v;
     }
 };
