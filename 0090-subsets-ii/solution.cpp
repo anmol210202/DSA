a7099af011f7b33143a9a41ddae01a7ae6bf5080
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-03-29 16:06:59 +0000

    [Sync]: Runtime - 3 ms (38.64%), Memory - 13.6 MB (20.35%)

--- 0090-subsets-ii/solution.cpp
+++ 0090-subsets-ii/solution.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void r(vector<int> &nums, int i,vector<int> tmp,set<vector<int>> &ans){
+    void r(vector<int> &nums, int i,vector<int> &tmp,set<vector<int>> &ans){
         if(i==nums.size()){
             ans.insert(tmp);
             return;
@@ -8,7 +8,7 @@ public:
         r(nums,i+1,tmp,ans);
         tmp.push_back(nums[i]);
         r(nums,i+1,tmp,ans);
-        // tmp.pop_back();
+        tmp.pop_back();
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         set<vector<int>> ans;
