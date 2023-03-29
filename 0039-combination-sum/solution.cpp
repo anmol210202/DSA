e7f4d1d8ea90f1e074cb7b2674a0fd281c9d9d19
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-03-29 18:08:02 +0000

    [Sync]: Runtime - 4 ms (43.53%), Memory - 10.8 MB (100.00%)

--- 0039-combination-sum/solution.cpp
+++ 0039-combination-sum/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    void r(vector<int> &can ,int i, int t,int sum,vector<int> &tmp,set<vector<int>> &ans){
-        if(sum==t) ans.insert(tmp);
-        if(i==can.size() or sum>t) return;
-        r(can,i+1,t,sum,tmp,ans);
-        tmp.push_back(can[i]);
-        r(can,i,t,sum+can[i],tmp,ans);
-        r(can,i+1,t,sum+can[i],tmp,ans);
-        tmp.pop_back();
+    void r(vector<int> &can ,int i, int t,vector<int> &tmp,vector<vector<int>> &ans){
+        if(t==0) {ans.push_back(tmp);return;}
+        for(int j=i; j<can.size(); j++){
+            if(can[i]>t) continue;
+            tmp.push_back(can[j]);
+            r(can,j,t-can[j],tmp,ans);
+            tmp.pop_back();
+        }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
         vector<int> tmp;
-        set<vector<int>> ans;
-        vector<vector<int>> a;
-        r(candidates,0,target,0,tmp,ans);
-        for(auto &e:ans) a.push_back(e);
-        return a;
+        vector<vector<int>> ans;
+        r(candidates,0,target,tmp,ans);
+        return ans;
     }
 };
