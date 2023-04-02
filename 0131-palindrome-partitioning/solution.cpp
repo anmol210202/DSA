08b29250e540950162e85d5f3f51592d29109f5d
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-04-02 06:42:49 +0000

    [Sync]: Runtime - 137 ms (13.23%), Memory - 75.7 MB (31.87%)

--- 0131-palindrome-partitioning/solution.cpp
+++ 0131-palindrome-partitioning/solution.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    bool c(string s, int st,int en){
-        while(st<=en){
-            if(s[st]!=s[en]) return false;
-            st++,en--;
+    vector<string> tmp;
+    vector<vector<string>> ans;
+    bool c(string &s,int i,int j){
+        while(i<j){
+            if(s[i]!=s[j]) return false;
+            else i++,j--;
         }
         return true;
     }
-    void bT(int i,string s, vector<vector<string>> &v,vector<string> &tmp){
-        if(i==s.size()) {v.push_back(tmp); return ;}
+    void r(string &s, int i){
+        if(i==s.size()){
+            ans.push_back(tmp);
+            return ;
+        }
         for(int j=i; j<s.size(); j++){
             if(c(s,i,j)){
                 tmp.push_back(s.substr(i,j-i+1));
-                bT(j+1,s,v,tmp);
+                r(s,j+1);
                 tmp.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> v;
-        vector<string> tmp;
-        bT(0,s,v,tmp);
-        return v;
+        r(s,0);
+        return ans;
     }
 };
