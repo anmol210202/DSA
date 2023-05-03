14d0bd3ee1932553e89b3598129bc5e8933ac584
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-05-03 13:24:09 +0000

    [Sync]: Runtime - 65 ms (6.06%), Memory - 32.3 MB (93.62%)

--- 1392-find-the-difference-of-two-arrays/solution.cpp
+++ 1392-find-the-difference-of-two-arrays/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         vector<vector<int>> v;
-        set<int> s1,s2;
+        unordered_set<int> s1,s2;
         for(auto &e:nums1) s1.insert(e);
         for(auto &e:nums2) s2.insert(e);
         vector<int> tmp;
