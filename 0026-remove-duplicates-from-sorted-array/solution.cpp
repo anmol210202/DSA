05bcca283e3af4b94ef379c89196f14298f3d8e9
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-09-08 15:19:19 +0000

    [Sync]: Runtime - 15 ms (7.41%), Memory - 18.5 MB (100.00%)

--- 0026-remove-duplicates-from-sorted-array/solution.cpp
+++ 0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i=0;
-        int j=1;
-        while(j<nums.size()){
-            if(nums[i]!=nums[j]){
-                nums[i+1]=nums[j];
-                i++;
-                j++;
-            } else j++;
+        if (nums.empty()) return 0;
+        int k = 1;         
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
         }
-        return ++i;
+        return k;
     }
 };
+
