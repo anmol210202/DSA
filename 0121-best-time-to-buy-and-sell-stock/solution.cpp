40d2ad0cff1d6629b667775d430876ee668efc1f
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-09-11 15:03:52 +0000

    [Sync]: Runtime - 95 ms (6.83%), Memory - 93.3 MB (99.98%)

--- 0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ 0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int mn = INT_MAX,a=0;
-        for(auto &e:prices) mn=min(mn,e),a=max(a,e-mn);
-        return a;
+        int mn = INT_MAX , profit = 0; 
+        for(auto &e:prices ) mn = min(mn , e) , profit = max(profit , e-mn);
+        return profit;
     }
 };
