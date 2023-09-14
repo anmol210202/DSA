3573d55123dd4073607eaa4838a9a4f0b9dba351
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-09-14 17:01:51 +0000

    [Sync]: Runtime - 4 ms (0.94%), Memory - 6.9 MB (100.00%)

--- 0058-length-of-last-word/solution.cpp
+++ 0058-length-of-last-word/solution.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int ans =0;
-        bool x=true;
-        for(int i=s.size()-1; i>=0; i--){
-            if(s[i]!=' '){
-                ans++;
-                x=false;
-            }else{
-                if(!x){
-                    return ans;
-                }
-                x=true;
+        int c = 0;
+        int n = s.size();
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] != ' ') {
+                c++;
+            } else if (c > 0) {
+                return c;
             }
         }
-        return ans;
+        
+        return c;
     }
 };
+
