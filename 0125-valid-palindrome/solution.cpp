85d06898f7fdd1efa681027c23a1f8da776a09d0
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-09-16 18:31:46 +0000

    [Sync]: Runtime - 4 ms (17.84%), Memory - 7.8 MB (100.00%)

--- 0125-valid-palindrome/solution.cpp
+++ 0125-valid-palindrome/solution.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    bool check(string &s,int l){
-        if(int(tolower(s[l]))>=97 and int(tolower(s[l]))<=122 || int(tolower(s[l]))>=48 and int(tolower(s[l]))<=57) return true;
-        return false;
-    }
-    bool isPalindrome(string &s) {
-        int l=0 , r=s.size()-1;
-        while(l<=r)
-            if(check(s,l) and check(s,r))
-                if(tolower(s[l])!=tolower(s[r])) return false;
-                else{
-                    l++; r--;
-                }
-            else if(check(s,l) and !check(s,r)) r--;
-            else if(!check(s,l)and check(s,r)) l++;
-            else{
-                l++; r--;
+    bool isPalindrome(string s) {
+        int i=0, j=s.size()-1;
+        while(i<j){
+            if(!isalnum(s[i])){
+                i++;
+                continue;
             }
+            if(!isalnum(s[j])){
+                j--;
+                continue;
+            }
+            if(tolower(s[i])!=tolower(s[j])) return false;
+            i++,j--;
+        }
         return true;
     }
 };
