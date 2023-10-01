ea83376edf0a86298db14e5e62d3f669eeb77d4c
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-10-01 11:22:21 +0000

    [Sync]: Runtime - 11 ms (5.07%), Memory - 17.7 MB (100.00%)

--- 0057-insert-interval/solution.cpp
+++ 0057-insert-interval/solution.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<pair<int,bool>> v(100000,{0,false});
-        vector<vector<int>> ans;
-        for(auto &e:intervals){
-            if(e[0]==e[1]) v[e[0]].second=true;
-            v[e[0]].first+=1;
-            v[e[1]].first+=-1;
+    vector<vector<int>> insert(vector<vector<int>>& in, vector<int>& newIn) {
+        int n = in.size() , i=0;
+        vector<vector<int>> vv;
+
+        while(i<n and in[i][1]<newIn[0]) {
+            vv.push_back(in[i++]);
         }
-        v[newInterval[0]].first+=1;
-        v[newInterval[1]].first+=-1;
-        for(int i=1; i<100000; i++) v[i].first+=v[i-1].first;
-        if(newInterval[0]==newInterval[1] and v[newInterval[0]].first==0) v[newInterval[0]].second=true;
-        vector<int> tmp;
-        if(v[0].first!=0 ) tmp.push_back(0);
-        if(v[0].second and v[0].first==0) ans.push_back({0,0});
-        for(int i=1; i<100000-1; i++) {
-            if(v[i].second and v[i].first ==0 and v[i-1].first==0) ans.push_back({i ,i});
-            if(v[i-1].first==0 and v[i].first!=0) tmp.push_back(i);
-            if(v[i-1].first!=0 and v[i].first==0){
-                tmp.push_back(i);
-                ans.push_back(tmp);
-                tmp.clear();
-            }
+
+        while(i<n and in[i][0]<=newIn[1]){
+            newIn[0]=min(newIn[0],in[i][0]);
+            newIn[1]=max(newIn[1],in[i][1]);
+            i++;
         }
-        return ans;
+        vv.push_back(newIn);
+
+        while(i<n) vv.push_back(in[i++]);
+        
+        return vv;
     }
 };
