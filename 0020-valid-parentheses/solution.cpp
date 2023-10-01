594ade28fc90c8452a3083e998d6a3c0ca53a58c
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-10-01 14:04:58 +0000

    [Sync]: Runtime - 3 ms (7.59%), Memory - 6.8 MB (100.00%)

--- 0020-valid-parentheses/solution.cpp
+++ 0020-valid-parentheses/solution.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> stack;
-        for(auto &x:s)
-            if(stack.empty()) stack.push(x);
-            else if(((x-stack.top())==2 or x-stack.top()==1) and (x-stack.top())!=0) stack.pop();
-            else stack.push(x);
-        return stack.empty() ? true : false;
+        stack<char> st;
+        int n= s.size();
+
+        for(int i=0; i<n; i++){
+            if(!st.empty()){
+                char temp= st.top();
+                // cout<<temp-s[i];
+                if(temp - s[i]==-1 or temp - s[i]==-2) st.pop();
+                else st.push(s[i]);
+            } else {
+                st.push(s[i]);
+            }
+        }
+
+        return st.empty();
     }
 };
