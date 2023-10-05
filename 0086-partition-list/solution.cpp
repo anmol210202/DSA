ee03bb24e86d1781272c75685751c7f5ca5ae7c6
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-10-05 17:41:53 +0000

    [Sync]: Runtime - 4 ms (2.26%), Memory - 10.7 MB (100.00%)

--- 0086-partition-list/solution.cpp
+++ 0086-partition-list/solution.cpp
@@ -1,33 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode* lessHead = new ListNode(); // Dummy node for the list with values less than x.
-        ListNode* greaterHead = new ListNode(); // Dummy node for the list with values greater than or equal to x.
-        ListNode* lessTail = lessHead;
-        ListNode* greaterTail = greaterHead;
-        
+        ListNode* zero = new ListNode();
+        zero->next = head;
+        ListNode* pre = zero;
         ListNode* curr = head;
 
-        while (curr != nullptr) {
-            if (curr->val < x) {
-                lessTail->next = curr;
-                lessTail = lessTail->next;
+
+        vector<int> v;
+
+        while(curr!=NULL){
+            if(curr->val>=x){
+                v.push_back(curr->val);
+                pre->next = curr->next;
+                curr = curr->next;
             } else {
-                greaterTail->next = curr;
-                greaterTail = greaterTail->next;
+                pre = curr;
+                curr=curr->next;
             }
-            curr = curr->next;
+        }
+        
+        for(auto &e:v){
+            ListNode* tmp = new ListNode(e);
+            pre->next = tmp;
+            pre = pre->next;
         }
 
-        // Connect the two lists.
-        lessTail->next = greaterHead->next;
-        greaterTail->next = nullptr;
-
-        ListNode* newHead = lessHead->next; // The new head is the first node in the list with values less than x.
-
-        delete lessHead; // Free the dummy nodes.
-
-        return newHead;
+        return zero->next;
     }
 };
-
