a01970d18bf9f93a89a50e81c1a628ff139aef20
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-10-10 19:04:11 +0000

    [Sync]: Runtime - 7 ms (2.09%), Memory - 21.5 MB (34.80%)

--- 0112-path-sum/solution.cpp
+++ 0112-path-sum/solution.cpp
@@ -11,23 +11,16 @@
  */
 class Solution {
 public:
-
-    bool traverse(TreeNode* root,int targetSum){
-        if(!root->left and !root->right){
-            if(targetSum - root->val == 0) return true;
-            return false;
-        }
-        if(root->left){
-            if(traverse(root->left,targetSum-root->val)) return true;
-        }
-        if(root->right){
-            if(traverse(root->right,targetSum-root->val)) return true;
-        }
-        return false;
-    }
-
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root) return false;
-        return traverse(root,targetSum);
+
+        targetSum-=root->val;
+
+        if(!root->left and !root->right) return targetSum == 0;
+        
+        bool leftPath = hasPathSum(root->left , targetSum);
+        bool rightPath = hasPathSum(root->right , targetSum);
+
+        return leftPath or rightPath;
     }
 };
