5ac70606fd2d58a5c2c68189bc5f32e8e4f4848f
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2025-03-24 13:13:22 +0000

    [Sync]: Runtime - 3 ms (73.25%), Memory - 14.9 MB (35.80%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n= nums.size();
-        vector<pair<int,int>> map(n);
-        for(int i=0; i<n; i++){
-            map[i]={nums[i],i};
-        }
-        sort(map.begin(),map.end());
-        int i=0;
-        int j=nums.size()-1;
-        while(i<j){
-            int sum=map[i].first+map[j].first;
-            if(sum==target){
-                return {map[i].second,map[j].second};
-            } else if(sum>target){
-                j--;
-            } else {
-                i++;
+        unordered_map<int,int> map;
+        for(int i=0; i<nums.size(); i++){
+            int cmt = target-nums[i];
+            if(map.find(cmt)!=map.end()){
+                return {map[cmt],i};
             }
+            map[nums[i]]=i;
         }
         return {};
     }
