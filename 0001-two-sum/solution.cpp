c98d6e05e9e18856461c60017a1caa5ac773a850
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2025-03-24 12:58:23 +0000

    [Sync]: Runtime - 3 ms (73.25%), Memory - 14.7 MB (48.76%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -1,19 +1,24 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> m;
-        for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-            if(m.find(complement) != m.end()) {
-                return { m[complement], i };
+        int n= nums.size();
+        vector<pair<int,int>> map(n);
+        for(int i=0; i<n; i++){
+            map[i]={nums[i],i};
+        }
+        sort(map.begin(),map.end());
+        int i=0;
+        int j=nums.size()-1;
+        while(i<j){
+            int sum=map[i].first+map[j].first;
+            if(sum==target){
+                return {map[i].second,map[j].second};
+            } else if(sum>target){
+                j--;
+            } else {
+                i++;
             }
-            m[nums[i]] = i;
         }
         return {};
     }
 };
-
