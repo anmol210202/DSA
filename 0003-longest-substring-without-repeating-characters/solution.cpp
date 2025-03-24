40c75f945af9d3bef28f64f308cfe78b70dd9f2a
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2025-03-24 19:32:37 +0000

    [Sync]: Runtime - 3 ms (83.69%), Memory - 12 MB (49.28%)

--- 0003-longest-substring-without-repeating-characters/solution.cpp
+++ 0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<int,int> m;
-        // int st=0,l=0;
-        // for(int i=0; i<s.size(); i++){
-        //     if(m.find(s[i])!=m.end() and m[s[i]]>=st) st=1+m[s[i]];
-        //     m[s[i]]=i;
-        //     l=max(l,i-st+1);
-        // }
-        // return l;
+        unordered_map<char,int> map;
+        int startPoint = 0;
+        int maxLength = 0;
 
-        int startingPoint=0,len=0,n=s.size();
-        for(int i=0; i<n; i++){
-            if(m.find(s[i])!=m.end() and m[s[i]]>=startingPoint) startingPoint = m[s[i]] + 1;
-            m[s[i]]=i;
-            len = max(len , i- startingPoint +1);
+        for(int i=0; i<s.size(); i++){
+            if(map.find(s[i])!=map.end() && startPoint<=map[s[i]] ){
+                startPoint = map[s[i]]+1;
+            }
+            map[s[i]]=i;
+            maxLength=max(maxLength,i-startPoint+1); 
         }
-
-        return len; 
+        return maxLength;
     }
 };
