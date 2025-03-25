a5920a29bff7f982f367ab4ade43c08c469fde0c
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2025-03-25 16:24:20 +0000

    [Sync]: Runtime - 0 ms (100.00%), Memory - 7.7 MB (96.89%)

--- 0202-happy-number/solution.cpp
+++ 0202-happy-number/solution.cpp
@@ -12,12 +12,24 @@ public:
     }
 
     bool isHappy(int n) {
-        unordered_set<int> box;
-        while(n!=1){
-            box.insert(n);
-            n=helper(n);
-            if(box.find(n)!=box.end()) return false;
+        // using slow fast pointer now
+        int fast = n;
+        int slow = n;
+        while(1){
+            slow = helper(slow);
+            fast = helper(helper(fast));
+
+            if(slow ==1 || fast == 1) return true;
+
+            if(fast == slow) return false;
         }
-        return true;
+
+        // unordered_set<int> box;
+        // while(n!=1){
+        //     box.insert(n);
+        //     n=helper(n);
+        //     if(box.find(n)!=box.end()) return false;
+        // }
+        // return true;
     }
 };
