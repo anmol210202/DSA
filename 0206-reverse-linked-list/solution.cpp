f62c677acda7639db94d0db719d1f221fc3f5929
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2025-03-26 12:53:51 +0000

    [Sync]: Runtime - 0 ms (100.00%), Memory - 13.6 MB (18.09%)

--- 0206-reverse-linked-list/solution.cpp
+++ 0206-reverse-linked-list/solution.cpp
@@ -11,15 +11,17 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev = NULL;
-        ListNode* curr = head;
+        if(!head || !head->next) return head;
+        ListNode *prev = NULL;
+        ListNode *curr = head;
 
-        while(curr!=NULL){
-            ListNode* next = curr->next;
+        while(curr){
+            ListNode *next = curr->next;
             curr->next = prev;
             prev = curr;
             curr = next;
         }
+
         return prev;
     }
 };
