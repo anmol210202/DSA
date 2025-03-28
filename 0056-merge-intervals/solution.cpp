eebb0ec880eb557207cf977767c56eebd80af292
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2025-03-28 16:49:01 +0000

    [Sync]: Runtime - 1 ms (95.37%), Memory - 23.8 MB (80.37%)

--- 0056-merge-intervals/solution.cpp
+++ 0056-merge-intervals/solution.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& in) {
-        sort(in.begin(), in.end());
-        int n = in.size();
-        vector<vector<int>> vv;
-        if(n==0) return vv;
-        int s=in[0][0],e=in[0][1];
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        int n=intervals.size();
+        
+        if(n<1) return intervals;
+        vector<vector<int>> merged(1,intervals[0]);
 
         for(int i=1; i<n; i++){
-            if(in[i][0]<=e){
-                e=max(e,in[i][1]);
-            } else {
-                vv.push_back({s,e});
-                s=in[i][0],e=max(e,in[i][1]);
+            if(merged.back()[1] >= intervals[i][0]){
+                merged.back()[1]=(merged.back()[1] > intervals[i][1]) ? merged.back()[1]:intervals[i][1];
+            } else{
+                merged.push_back(intervals[i]);
             }
         }
-        vv.push_back({s,e});
-
-        return vv;   
+        return merged;
     }
 };
