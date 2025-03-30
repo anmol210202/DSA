07bf14d5cfa7aadb00cd1e28dbf38eac43c4e055
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2025-03-30 13:37:07 +0000

    [Sync]: Runtime - 41 ms (77.70%), Memory - 94 MB (67.72%)

--- 0435-non-overlapping-intervals/solution.cpp
+++ 0435-non-overlapping-intervals/solution.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),[](vector<int>& a,vector<int>& b){
+        if(intervals.empty()) return 0;
+        sort(intervals.begin(),intervals.end(),[](const vector<int>& a,const vector<int>& b){     // const to add unessary copies of vector 
             return a[1] < b[1];
         });
-
         int notOverlap = 1,end=intervals[0][1],n=intervals.size();
         
-        for(int i=1; i<n; i++){
+        for(auto i=1; i<n; i++){
             if(end <= intervals[i][0]){
                 notOverlap++;
                 end = intervals[i][1];
