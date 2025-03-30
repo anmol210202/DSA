9bf7faa5f9cb81c4b2b6d92b00e87208a3ea0d10
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2025-03-30 15:28:15 +0000

    [Sync]: Runtime - 0 ms (100.00%), Memory - 15 MB (95.19%)

--- 0033-search-in-rotated-sorted-array/solution.cpp
+++ 0033-search-in-rotated-sorted-array/solution.cpp
@@ -1,17 +1,23 @@
 class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int s=0,e=nums.size()-1,m=s+(e-s)/2;
-        while(s<=e){
-            if(nums[m]==target) return m;
-            if(nums[m]>=nums[s]){
-                if(nums[m]>=target and nums[s]<=target) e=--m;
-                else s=++m;
-            } else{
-                if(nums[e]>=target and nums[m]<=target) s=++m;
-                else e=--m;
+    public:
+        int search(vector<int>& nums, int target) {
+            int left = 0, rgt = nums.size() - 1;
+            while (left <= rgt) {
+                int mid = left + (rgt - left) / 2;
+                if (nums[mid] == target)
+                    return mid;
+                
+                if (nums[left] <= nums[mid]) {
+                    // If target is within the sorted left half
+                    if (target >= nums[left] && target < nums[mid]) rgt = mid - 1;
+                    else left = mid + 1;
+                } else {
+                    // Right half is sorted
+                    if (target > nums[mid] && target <= nums[rgt]) left = mid + 1;
+                    else rgt = mid - 1;
+                }
             }
+            return -1;
         }
-        return -1;
-    }
-};
+    };
+    
